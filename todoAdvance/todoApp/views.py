b5b300cad222f55a1e8e2dea7b2f042a7d3fe4f7
Abhishek Singh <avsekjob@gmail.com>
2022-10-14 23:51:22 +0530

    .

--- todoAdvance/todoApp/views.py
+++ todoAdvance/todoApp/views.py
@@ -12,8 +12,8 @@ def add(request):
     title, description, lastModified,submit, add, modify, delete = "", "", "", "", "", "", ""
     if request.GET:
         add = request.GET['add']
-        modify = request.GET['modify']
-        delete = request.GET['delete']
+        # modify = request.GET['modify']
+        # delete = request.GET['delete']
 
         title = request.GET['title']
         description = request.GET['description']
@@ -22,7 +22,7 @@ def add(request):
         store = Todo(title = title, description = description, last_modified = lastModified)
         store.save()
         updated = Todo.objects.all()
-        return render(request, "add.html", {'title':title, 'description':description, 'lastModified':lastModified,"updated": updated})
+        return render(request, "add.html", {'title':title, 'description':description, 'lastModified':lastModified, "updated":updated})
     return render(request, "add.html")
 
 
